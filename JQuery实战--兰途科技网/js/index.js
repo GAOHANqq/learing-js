fea8e31ee7458c7d1c4a8402d791a0567ad1d913
GAOHANqq <1271448267@qq.com>
2018-11-23 19:19:34 +0800

    JQuery实战--响应式兰途科技网

--- JQuery实战--兰途科技网/js/index.js
+++ JQuery实战--兰途科技网/js/index.js
@@ -1,10 +1,36 @@
 
 (function(){
     var $more = $("#header .more");
+    var $tab = $("#banner").find(".b-tab li");
+    var $bg = $("#banner").find(".b-part .part");
+    var index = 0;
+    var length = $tab.length;
+
+    // 更多的点击事件
     $more.click(function(){
         $(this).find(".more-hide").toggle();
     });
 
+    // tab点击事件
+    $tab.click(function(){
+        index  = $(this).index();
+        change(index);
+    });
+
+    // 轮播
+    setInterval(function(){
+        index ++;
+        if( index === length-1  ){
+            index = 0;
+        }
+        change(index);
+    },3000)
+
+    // 变化函数
+    function change(index){
+        $tab.eq(index).addClass("active").siblings().removeClass("active");
+        $bg.eq(index).show().siblings().hide();
+    }
 })();
 
 

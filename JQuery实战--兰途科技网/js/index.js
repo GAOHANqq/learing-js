5d9b5a9196a5c5864d4c99cd2316c77da20af0b9
GAOHANqq <1271448267@qq.com>
2018-11-24 23:03:21 +0800

    JQuery实战--响应式兰途科技网

--- JQuery实战--兰途科技网/js/index.js
+++ JQuery实战--兰途科技网/js/index.js
@@ -1,8 +1,7 @@
-
 (function(){
     // header区域
     (function(){
-        var $more = $("#header .more");
+        let $more = $("#header .more");
 
         // 更多的点击事件
         $more.click(function(){
@@ -12,12 +11,13 @@
 
     // banner区域
     (function(){
-        var $banner = $("#banner")
-        var $tab = $banner.find(".b-tab li");
-        var $bg = $banner.find(".b-part .part");
-        var index = 0;
-        var length = $tab.length;
-        var timer;
+        let $banner = $("#banner"),
+            $tab = $banner.find(".b-tab li"),
+            $bg = $banner.find(".b-part .part"),
+            index = 0,
+            length = $tab.length,
+            timer
+        ;
 
         // tab点击事件
         $tab.click(function(){
@@ -47,24 +47,94 @@
 
     // classic区域
     (function(){
-        var $classic = $("#classic");
-        var $li = $classic.find(".c-content .c-pic li");
-        var $btn = $classic.find(".c-pic .c-btn .btn");
-        var length = $li.length;
+        let $classic = $("#classic"),
+            $ul = $classic.find(".c-content .c-pic ul"),
+            $li = $ul.children(),
+            $btn = $classic.find(".c-pic .c-btn .btn"),
+            $tab = $classic.find(".c-content .c-tab ul li"),
+            width = $li.first().width(),
+            length = $li.length,
+            midIndex = Math.floor(length/2),
+            clickTime = 0,
+            sumWidth = 0,
+            timer
+        ;
+        changeClassName();
+        setTimeout(function(){
+            sumWidth = 0;
+            $li.each(function(){
+                sumWidth += $(this).width();
+            });
+            $ul.css({
+                "marginLeft":-sumWidth/2,
+                "opacity" : 1
+            });
+            $tab.eq(midIndex).addClass("mid");
 
-        var midIndex = Math.floor(length/2);
+        },300);
+
+        $(window).resize(function(){
+            clearTimeout(timer);
+            timer = setTimeout(function(){
+                sumWidth = 0;
+                width = $ul.children().first().width();
+                $li.each(function(){
+                    sumWidth += $(this).width();
+                });
+                $ul.stop().animate({"marginLeft":-sumWidth/2},300);
+            },300);
+        });
+
+        // 左右按钮事件
+        $btn.click(function(){
+            if(new Date() - clickTime < 350) return;
+            clickTime = new Date();
+            if( $(this).index() ){
+                midIndex ++;
+                midIndex %= length;
+                $ul.stop().animate({
+                        "marginLeft" :  -sumWidth/2 - width
+                },300,function(){
+                    $(this)
+                        .css("marginLeft",-sumWidth/2)
+                        .append($(this).children().first());
+                });
+
+            }else{
+                midIndex --;
+                if( midIndex<0 ){
+                    midIndex = length - 1;
+                }
+                $ul.stop().animate({
+                        "marginLeft" : -sumWidth/2 + width
+                },300,function(){
+                    $(this)
+                        .css("marginLeft",-sumWidth/2)
+                        .prepend($(this).children().last());
+                });
+            }
+            changeClassName();
+        });
+
+        // 变化函数
+        function changeClassName(){
+            let [a,b,c] = [midIndex,midIndex+1,midIndex-1];
+            if(b>=length) b=0;
+            if(c<0) c=length-1;
+            $tab.removeClass("active");
+            $li.removeClass("mid side");
+            $li.eq(a).addClass("mid");
+            $li.eq(b).addClass("side");
+            $li.eq(c).addClass("side");
+            $tab.eq(midIndex).addClass("active");
+        }
 
 
-        $li.each(function(){
-            var index = $(this).index();
-            $li.eq(midIndex).addClass("mid");
-            $li.eq(midIndex-1).addClass("side");
-            $li.eq(midIndex+1).addClass("side");
-            $(this).css({
-                "left" : 200*index + "px"
-            })
-        })
+
     })();
+
+
+
 
 })();
 

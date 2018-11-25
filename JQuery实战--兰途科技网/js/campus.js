9b989ade49cb3313c052d076b911da2ec6a42ab2
GAOHANqq <1271448267@qq.com>
2018-11-25 23:01:40 +0800

    JQuery实战--响应式兰途科技网

--- JQuery实战--兰途科技网/js/campus.js
+++ JQuery实战--兰途科技网/js/campus.js
@@ -1,15 +1,23 @@
 (function(){
     let $content  =$("#content"),
-        $part1 = $content.find("#part1")
+        $part = $content.find(".part"),
+        $next = $content.find(".next"),
+        $part1 = $content.find("#part1"),
+        index = 0,
+        length = $part.length;
     ;
+    // part1的高度
     $(window).resize(part1H);
     part1H();
     function part1H(){
         $part1.height($(window).height()-parseFloat($content.css("margin")));
     };
 
-
-
+    // next的点击事件
+    $next.click(function(){
+        index = $(this).index();
+        $part.eq( index+1 ).slideDown(300).end().eq( index).slideUp(300);
+    });
 })();
 
 

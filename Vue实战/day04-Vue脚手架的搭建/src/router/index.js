cc42f17dd7e2b1ad60e905d9b7bf2738e08af6fa
GAOHANqq <1271448267@qq.com>
2018-12-07 18:22:54 +0800

    Vue脚手架的搭建

--- Vue实战/day04-Vue脚手架的搭建/src/router/index.js
+++ Vue实战/day04-Vue脚手架的搭建/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld' // @代表 src目录 没有提示
+import HelloWorld from '../components/HelloWorld' // 相对路径有提示
 
 Vue.use(Router)
 

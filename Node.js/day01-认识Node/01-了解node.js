2d7c16982fc828fd53fa8afcf2eef0c49a027fce
GAOHANqq <1271448267@qq.com>
2018-12-08 21:55:46 +0800

    认识Node 安装与测试

---
+++ Node.js/day01-认识Node/01-了解node.js
@@ -0,0 +1,42 @@
+
+/*
+
+Node.js的优势:高并发
+I/O 密集型  : 请求多,处理少
+CPU 密集型  : 请求少,处理多
+
+Node.js 是一个基于ChromeV8引擎的JavaScript运行环境
+Node,js 使用了一个事件驱动,非阻塞式的I/O的模型,使其轻量又高效
+
+原生JS       DOM操作
+Node.js     没有DOM相关的内容
+
+Node.js是服务端的JavaScript  顶层对象是 global
+
+
+命令行运行node -v测试是否安装完成；
+使用node 文件名运行一个js文件；
+
+进入node REPL(交互式解析器)直接运行代码。
+退出时 连按两次 Ctrl + c
+
+
+命令行: Ctrl + l 光标置顶
+在WebStorm中,使用 Alt + F12 打开/关闭命令行
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
